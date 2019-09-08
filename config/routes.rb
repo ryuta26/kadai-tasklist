c8a46ee8882f091287738c51b61ec1b4f452e5d4
ryu.furu <fulufulu6278@gmail.com>
2019-09-08 12:47:46 +0000

    correction2

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'users/show'
   get 'users/new'
   get 'users/create'
-  get 'toppages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
